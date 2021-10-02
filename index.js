4cc7f389649be4647c21d91a28403b3dfa1c5768
Jason Lam <32461232+jlam2@users.noreply.github.com>
2021-10-02 22:32:58 +0000

    Added Read endpoints for users and tasks

--- index.js
+++ index.js
@@ -14,18 +14,52 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         res.send(user)
     }).catch((err) => {
-        res.status(400).send(err)
+        res.sendStatus(400)
     })
 })
 
+app.get('/users', (req, res) =>{
+    User.find().then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        if(!user) return res.sendStatus(404)
+        res.send(user)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+})
+
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
-    user.save().then(() => {
+    task.save().then(() => {
         res.send(task)
     }).catch((err) => {
         res.status(400).send(task)
     })
 })
 
+app.get('/tasks', (req, res) => {
+    Task.find().then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if(!task) return res.sendStatus(404)
+        res.send(task)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+})
 
 app.listen(port, () => console.log(`Server is up on port ${port}`) )
