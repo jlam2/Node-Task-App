9310f0cd559d98d64f19c60ca028a4207d8e9627
kastilas <>
2021-10-15 00:26:28 +0000

    added avatar field

--- models/user.js
+++ models/user.js
@@ -41,7 +41,10 @@ const userSchema = new mongoose.Schema(
                 type: String,
                 required: true
             }
-        }]
+        }],
+        avatar:{
+            type: Buffer
+        }
     },
     {
         timestamps: true
